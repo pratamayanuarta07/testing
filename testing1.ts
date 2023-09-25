ed89cc4c6eee54648fd1489cf586c4760ba2ba41
pratamayanuarta07 <pratamayanuarta@gmail.com>
2023-09-25 11:19:29 +0700

    Menghapus Soal Versi Lawas

--- testing1.ts
+++ testing1.ts
@@ -1,45 +1,3 @@
-const soal1 = (pesan: string) =>
-{
-    var banyak = 0;
-for (let i = 0; i < pesan.length; i+=3) {
-    if (pesan.toUpperCase().charAt(i) != 'S') {
-        //console.log(pesan.toUpperCase().charAt(i));
-        banyak ++;
-    }
-    if (pesan.toUpperCase().charAt((i+1)) != 'O') {
-        //console.log(pesan.toUpperCase().charAt(i+1));
-        banyak++;
-    }
-    if (pesan.toUpperCase().charAt((i+2)) != 'S') {
-        //console.log(pesan.toUpperCase().charAt(i+1));
-        banyak++;
-    }
-    
-}
-return banyak;
-}
-
-const soal2 = (bill:number[], k:number, b:number) =>
-{
-    var tot = bill.reduce((total, c, i) => 
-    {
-    if (i != k) {
-        total+= c;
-    }
-    return total;  
-    },0);
-    var last = tot/2;
-    if (last < b) {
-        var true_tot =Math.abs((tot/2) - b);
-        console.log(true_tot);    
-    }
-    else{
-        console.log('Bon Appetti');
-    }
-}
-
-
-
 const soal1ver2 = (pesan: string):any =>
 {
     try {
@@ -106,5 +64,5 @@ const soal2ver2 = (bill:number[], k:number, b:number) =>
         console.log(error.message);
     }
 }
-soal2ver2([3,10,2,9],1,25);
+soal2ver2([3,10,2,9],1,12);
 // console.log(soal1ver2('sossso'));
